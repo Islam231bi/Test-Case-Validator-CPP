173d2f67598f717d478147e9e33bd6f63b9e7f56
Islam Bassuni <Islambassuni@gmail.com>
2022-07-18 15:48:03 +0200

    Runtime error and time limit exceeded handling.

--- test.sh
+++ test.sh
@@ -10,29 +10,36 @@ g++ $CPPFILE -o $OBJFILE
 RETVAL=$?
 correct=0
 count=0
+RUNERR=0
 
 if [ $RETVAL -ne 0 ]
 then
    echo "Compilation Error"
 else
-   ./$OBJFILE < $TESTCASE > "out.txt"
-
-   while read -u 3 -r line1 && read -u 4 -r line2; do
-      ((count++))
-      if [[ $line1 == $line2 ]]
-      then
-	 ((correct++))
-      fi
-   done 3<$ANSFILE 4<"out.txt"
-
-   echo "Correct cases: $correct"
-   let "val=$count-$correct"
-   echo "Wrong cases: $val"
-
-   if [ $correct -eq $count ]
+	timeout 5 ./$OBJFILE < $TESTCASE > "out.txt" || ((RUNERR++))
+   if [ $RUNERR -eq 0 ]
    then
-      echo "Successful Submission"
+   	while read -u 3 -r line1 && read -u 4 -r line2; do
+      		((count++))
+      		if [ $line1 == $line2 ]
+      		then
+	 		((correct++))
+      		fi
+   	done 3<$ANSFILE 4<"out.txt"
+
+   	echo "Correct cases: $correct"
+   	let "val=$count-$correct"
+   	echo "Wrong cases: $val"
+
+   	if [ $correct -eq $count ]
+   	then
+      		echo "Successful Submission"
+      		#echo "$mytime"
+   	else
+      		echo "Wrong Submission"
+      		#echo "$mytime"
+   	fi
    else
-      echo "Wrong Submission"
+	echo "RunTime error"
    fi
 fi
